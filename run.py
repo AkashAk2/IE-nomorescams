0e150a0306aabca9eea848dfefd447006393af7f
abal0041 <abal0041@student.monash.edu>
2023-09-05 03:44:24 +1000

    added password

--- run.py
+++ run.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 # Generate the secret_key each time
 app.secret_key = secrets.token_hex(16)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if 'authenticated' in session and session['authenticated']:
+        return render_template('index.html')  # index.html is the default page
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if password == 'Monash@27':  # website password
+            session['authenticated'] = True
+            return redirect(url_for('index'))
+        else:
+            flash('Wrong password. Please try again.')
+    return render_template('password_prompt.html')
+
+
+
 # Test connection to the database
 @app.route('/test-connection')
 def test_connection():
@@ -18,7 +33,8 @@ def test_connection():
         driver = '{ODBC Driver 17 for SQL Server}'
         
         connection_string = (f'DRIVER={driver};SERVER={server};DATABASE={database};'
-                             f'UID={username};PWD={password};Encrypt=True;TrustServerCertificate=False')
+                     f'UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=False')
+
         
         cnx = pyodbc.connect(connection_string)
         cnx.close()
